852d1cc86d6934a737d915106f9617a230b4326c
netopir <neto_pir@hotmail.com>
2022-08-18 19:13:22 +0200

    Update Blu-Ray_to_RAR_and_PAR2.bat

--- Blu-Ray_to_RAR_and_PAR2.bat
+++ Blu-Ray_to_RAR_and_PAR2.bat
@@ -1,4 +1,5 @@
 @echo off 
+REM 
 REM Path to WinRAR executable in Program Files. Change if location is different
 REM ---------------------- WinRAR Directory ------------------------------------
 set rardirectory="C:\Program Files\WinRAR\Rar.exe"
@@ -12,38 +13,61 @@ REM ---------------------- Par2 Directory --------------------------------------
 set par2directory="C:\Users\Martin\Desktop\ngPost_bluray\par2.exe"
 REM ----------------------------------------------------------------------------
 REM 
+REM ---------------------- SFV Directory ---------------------------------------
+set sfvdirectory="C:\Users\Martin\Desktop\ngPost_bluray\quicksfv.exe"
+REM ----------------------------------------------------------------------------
+REM 
 REM ---------------------- Source Directory ------------------------------------
-echo "Source phat"
+echo "Source Path"
 set /p source=" "
 REM ----------------------------------------------------------------------------
 REM 
 REM ---------------------- Folder Name -----------------------------------------
-echo "Folder name"
+echo "Folder Name"
 set /p foldername=" "
 REM ----------------------------------------------------------------------------
 REM 
 REM ---------------------- Arcive Name -----------------------------------------
-echo "Arcive name"
+echo "Arcive Name"
 set /p archivename=" "
 REM ----------------------------------------------------------------------------
 REM 
+REM ---------------------- Arcive Password -------------------------------------
+echo "Arcive Password"
+set /p archivepassword=" "
+REM ----------------------------------------------------------------------------
+REM 
 REM ---------------------- Destination Directory -------------------------------
-echo "Destination phat"
+echo "Destination Path"
 set destination="C:\Users\Martin\Desktop\ngPost_bluray\tmp\%foldername%"
 REM ----------------------------------------------------------------------------
 REM 
 REM ---------------------- Check Directory -------------------------------------
 if not exist %destination% mkdir %destination%
 REM ----------------------------------------------------------------------------
-
-%rardirectory% a -ep1 -r -idp -m0 -v100m %destination%\%archivename% %source%
-
+REM 
+REM ----------------------------------------------------------------------------
+cd %destination%
+REM ----------------------------------------------------------------------------
+REM 
+REM ----------------------------------------------------------------------------
+%rardirectory% a -ep1 -r -idp -m0 -v100m -p%archivepassword% %destination%\%archivename% %source%
+REM ----------------------------------------------------------------------------
+REM 
+REM ----------------------------------------------------------------------------
 REM %par2directory% c -ls -m1024 -rr15 %destination%\%archivename% %destination%\%archivename%*.rar
-
+REM ----------------------------------------------------------------------------
+REM 
+REM ----------------------------------------------------------------------------
 %par2directory% c -l -q -m1024 -r15 %destination%\%archivename% %destination%\%archivename%*.rar
-
+REM ----------------------------------------------------------------------------
+REM 
+REM ----------------------------------------------------------------------------
+%sfvdirectory% -c %archivename%.sfv *.*
+REM ----------------------------------------------------------------------------
+REM 
 pause
-
+REM 
 REM --------------------------- exit -------------------------------------------
-
+REM 
 EXIT /B 0
